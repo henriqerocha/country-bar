650f0bdff1b793c83b7a97175887d71d95cb0f2f
henriqerocha <44968212+henriqerocha@users.noreply.github.com>
2023-07-19 20:22:36 -0300

    correção scripts slide

--- src/componentes/carrousel/index.js
+++ src/componentes/carrousel/index.js
@@ -1,23 +1,66 @@
-import React from 'react';
+import react, { useEffect } from 'react';
 import './style.css';
 
-export default function Carrousel(){
+const MeuComponente = () => {
+    useEffect(() => {
+    const slideContainer = document.querySelector(".slides-container");
+    const slides = document.querySelectorAll(".slide");
+    const prevBtn = document.querySelector(".prev-button");
+    const nextBtn = document.querySelector(".next-button");
+  
+    let currentSlide = 0;
+
+    function showSlide(slideIndex) {
+        slideContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
+        currentSlide = slideIndex;
+      };
+
+    function showPrevSlide() {
+        if (currentSlide > 0) {
+            showSlide(currentSlide - 1);
+          } else {
+            showSlide(slides.length - 1); // Vai para o último slide
+          }
+      }
+  
+    function showNextSlide() {
+        if (currentSlide < slides.length - 1) {
+            showSlide(currentSlide + 1);
+        } else {
+            showSlide(0); // Volta para o primeiro slide
+            }
+      }
+  
+    prevBtn.addEventListener("click", showPrevSlide);
+    nextBtn.addEventListener("click", showNextSlide);
+  
+      // Importante: Remover o evento de clique ao desmontar o componente
+      return () => {
+        prevBtn.removeEventListener("click", showPrevSlide);
+        nextBtn.removeEventListener("click", showNextSlide);
+      };
+    }, []); // O array vazio assegura que o efeito é executado apenas uma vez, equivalente a componentDidMount
+    
     return(
-        <div class="slideshow">
-            <div class="slides-container">
-                <div class="slide">
-                    <img src="./assets/bebida1.jpg" alt="Imagem 1"></img>
-                </div>
-                <div class="slide">
-                    <img src="./assets/bebida2.jpg" alt="Imagem 2"></img>
-                </div>
-                <div class="slide">
-                    <img src="./assets/bebida3.jpg" alt="Imagem 3"></img>
-                </div>
-                    {/* <!-- Você pode adicionar mais imagens aqui, o layout não será quebrado --> */}
-            </div>        
-                <button class="prev-button">&#10094;</button>
-                <button class="next-button">&#10095;</button>
-        </div>
+        <section className='container-slideshow'>
+            <div className="slideshow">
+                <div className="slides-container">
+                    <div className="slide">
+                        <img src="./assets/bebida1.jpg" alt="Imagem 1"></img>
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/bebida2.jpg" alt="Imagem 2"></img>
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/bebida3.jpg" alt="Imagem 3"></img>
+                    </div>
+                        {/* <!-- Você pode adicionar mais imagens aqui, o layout não será quebrado --> */}
+                </div>        
+                    <button className="prev-button">&#10094;</button>
+                    <button className="next-button">&#10095;</button>
+            </div>
+        </section>
     );
 }
+
+export default MeuComponente;
--- src/scripts.js
+++ src/scripts.js
@@ -1,31 +1,32 @@
-    const slideContainer = document.querySelector(".slides-container");
-    const slides = document.querySelectorAll(".slide");
-    const prevBtn = document.querySelector(".prev-btn");
-    const nextBtn = document.querySelector(".next-btn");
+// document.addEventListener("DOMContentLoaded", function() {   
+//     const slideContainer = document.querySelector(".slides-container");
+//     const slides = document.querySelectorAll(".slide");
+//     const prevBtn = document.querySelector(".prev-button");
+//     const nextBtn = document.querySelector(".next-button");
   
-    let currentSlide = 0;
+//     let currentSlide = 0;
   
-    function showSlide(slideIndex) {
-      slideContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
-      currentSlide = slideIndex;
-    }
+//     function showSlide(slideIndex) {
+//       slideContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
+//       currentSlide = slideIndex;
+//     };
   
-    function showNextSlide() {
-      if (currentSlide < slides.length - 1) {
-        showSlide(currentSlide + 1);
-      } else {
-        showSlide(0); // Volta para o primeiro slide
-      }
-    }
+//     function showNextSlide() {
+//       if (currentSlide < slides.length - 1) {
+//         showSlide(currentSlide + 1);
+//       } else {
+//         showSlide(0); // Volta para o primeiro slide
+//       }
+//     }
   
-    function showPrevSlide() {
-      if (currentSlide > 0) {
-        showSlide(currentSlide - 1);
-      } else {
-        showSlide(slides.length - 1); // Vai para o último slide
-      }
-    }
+//     function showPrevSlide() {
+//       if (currentSlide > 0) {
+//         showSlide(currentSlide - 1);
+//       } else {
+//         showSlide(slides.length - 1); // Vai para o último slide
+//       }
+//     }
   
-    prevBtn.addEventListener("click", showPrevSlide);
-    nextBtn.addEventListener("click", showNextSlide);
-  
+//     prevBtn.addEventListener("click", showPrevSlide);
+//     nextBtn.addEventListener("click", showNextSlide);
+// });
