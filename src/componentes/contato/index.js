5960610ce40755381eb61cb7c0f882882a5bcea0
Henrique <henriiqesilva@gmail.com>
2023-07-21 01:51:10 -0300

    criação componente contato

--- src/componentes/contato/index.js
+++ src/componentes/contato/index.js
@@ -5,7 +5,29 @@ export default function Contato(){
     return(
         <section id='contato'>
             <div className='container-contato'>
-                <h2>Contato <img src='./assets/contato-emoticon.png' className='contato-emoticon'></img></h2>
+                <div className='titulo-contato'>
+                    <h2>Contato <img src='./assets/contato-emoticon.png' className='contato-emoticon'></img></h2>
+                </div>
+
+                <div className='form-container'>
+                    <form className='form-contato' method='POST' action=''>
+                        <div className='box-input'>
+                            <input type='text' className='input-button'></input>
+                            <label className='input-label'>Nome:</label>
+                        </div>
+
+                        <div className='box-input'>
+                            <input type='text' className='input-button'></input>
+                            <label className='input-label'>E-mail:</label>
+                        </div>
+
+                        <div className='box-input'>
+                            <textarea placeholder='Mensagem'></textarea>
+                        </div>
+
+                        <button type='submit'>Enviar</button>
+                    </form>
+                </div>
             </div>
         </section>
     );
