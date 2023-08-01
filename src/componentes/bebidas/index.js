b54c497f8f0bf4f533d9e702cdb9f08b94a8dfc6
Henrique <henriiqesilva@gmail.com>
2023-08-01 00:55:54 -0300

    arrumando slide

--- src/componentes/bebidas/index.js
+++ src/componentes/bebidas/index.js
@@ -6,7 +6,7 @@ export default function Bebidas(){
         <section id='bebidas'>
             <div className='container-bebidas'>
                 <div className='titulo-section'>
-                    <h2>Drinks <img src='./assets/bebidas-emoticon.webp'></img></h2>
+                    <h2>Drinks <img src='./assets/bebidas-emoticon.webp' alt='Emoticon bebidas'></img></h2>
                     <p>Nós temos os melhores e mais saborosos drinks para seu evento, veja alguns de nossos principais drinks abaixo. Ah, e tem muito mais!!!</p>
                     <p>Solicite já nosso orçamento para seu evento.</p>
                     <div className='button'>
@@ -17,7 +17,7 @@ export default function Bebidas(){
                 <div className='content-cards'>
                     <div className='card-bebidas'>
                         <div className='img-bebida'>
-                            <img src='./assets/bebida-1.webp'></img>  
+                            <img src='assets/bebida-1.webp' alt='Bebida pina colada'></img>  
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Pina Colada</h3>
@@ -34,7 +34,7 @@ export default function Bebidas(){
 
                     <div className='card-bebidas row-reverse'>
                         <div className='img-bebida'>
-                            <img src='./assets/bebida-2.webp'></img>  
+                            <img src='./assets/bebida-2.webp' alt='Bebida fresh green'></img>  
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Fresh Green</h3>
@@ -51,7 +51,7 @@ export default function Bebidas(){
 
                     <div className='card-bebidas'>
                         <div className='img-bebida'>
-                            <img src='./assets/bebida-3.webp'></img>  
+                            <img src='./assets/bebida-3.webp' alt='Bebida Mojito'></img>  
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Mojito</h3>
@@ -69,7 +69,7 @@ export default function Bebidas(){
 
                     <div className='card-bebidas row-reverse'>
                         <div className='img-bebida'>
-                            <img src='./assets/bebida-4.webp'></img>  
+                            <img src='./assets/bebida-4.webp' alt='Bebida Atlântico'></img>  
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Atlântico</h3>
--- src/componentes/carrousel/index.js
+++ src/componentes/carrousel/index.js
@@ -1,69 +1,86 @@
-import react, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-const MeuComponente = () => {
-    useEffect(() => {
+const Carrousel = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
     const slideContainer = document.querySelector(".slides-container");
     const slides = document.querySelectorAll(".slide");
     const prevBtn = document.querySelector(".prev-button");
     const nextBtn = document.querySelector(".next-button");
-  
-    let currentSlide = 0;
 
-    function showSlide(slideIndex) {
-        slideContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
-        currentSlide = slideIndex;
-      };
+    const showSlide = (slideIndex) => {
+      slideContainer.style.transition = "transform 0.5s ease-in-out";
+      slideContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
+      setCurrentSlide(slideIndex);
+    };
 
-    function showPrevSlide() {
+    const showPrevSlide = () => {
+      if (!isTransitioning) {
+        setIsTransitioning(true);
+        setTimeout(() => {
+          setIsTransitioning(false);
+        }, 500);
         if (currentSlide > 0) {
-            showSlide(currentSlide - 1);
-          } else {
-            showSlide(slides.length - 1); // Vai para o último slide
-          }
-      }
-  
-    function showNextSlide() {
-        if (currentSlide < slides.length - 1) {
-            showSlide(currentSlide + 1);
+          showSlide(currentSlide - 1);
         } else {
-            showSlide(0); // Volta para o primeiro slide
-            }
+          showSlide(slides.length - 1);
+        }
       }
-  
+    };
+
+    const showNextSlide = () => {
+      if (!isTransitioning) {
+        setIsTransitioning(true);
+        setTimeout(() => {
+          setIsTransitioning(false);
+        }, 500);
+        if (currentSlide < slides.length - 1) {
+          showSlide(currentSlide + 1);
+        } else {
+          showSlide(0);
+        }
+      }
+    };
+
+    const interval = setInterval(showNextSlide, 8000); // Alterna os slides a cada 8 segundos
+
     prevBtn.addEventListener("click", showPrevSlide);
     nextBtn.addEventListener("click", showNextSlide);
-  
-      // Importante: Remover o evento de clique ao desmontar o componente
-      return () => {
-        prevBtn.removeEventListener("click", showPrevSlide);
-        nextBtn.removeEventListener("click", showNextSlide);
-      };
-    }, []); // O array vazio assegura que o efeito é executado apenas uma vez, equivalente a componentDidMount
-    
-    return(
-        <section className='container-slideshow'>
-            <div className="slideshow">
-                <div className="slides-container">
-                    <div className="slide">
-                        <img src="./assets/bebida1.webp" alt="slide um"></img>
-                    </div>
-                    <div className="slide">
-                        <img src="./assets/bebida2.webp" alt="slide dois"></img>
-                    </div>
-                    <div className="slide">
-                        <img src="./assets/bebida3.webp" alt="slide tres"></img>
-                    </div>
-                    <div className="slide">
-                        <img src="./assets/bebida4.webp" alt="slide quatro"></img>
-                    </div>
-                        {/* <!-- Você pode adicionar mais imagens aqui, o layout não será quebrado --> */}
-                </div>        
-                    <button className="prev-button">&#10094;</button>
-                    <button className="next-button">&#10095;</button>
-            </div>
-        </section>
-    );
+
+    // Limpa o intervalo do temporizador ao desmontar o componente
+    return () => {
+      clearInterval(interval);
+      prevBtn.removeEventListener("click", showPrevSlide);
+      nextBtn.removeEventListener("click", showNextSlide);
+    };
+  }, [currentSlide, isTransitioning]);
+
+  return (
+    <section className='container-slideshow'>
+      <div className="slideshow">
+        <div className="slides-container">
+          <div className="slide">
+            <img src="./assets/bebida1.webp" alt="slide um"></img>
+          </div>
+          <div className="slide">
+            <img src="./assets/bebida2.webp" alt="slide dois"></img>
+          </div>
+          <div className="slide">
+            <img src="./assets/bebida3.webp" alt="slide tres"></img>
+          </div>
+          <div className="slide">
+            <img src="./assets/bebida4.webp" alt="slide quatro"></img>
+          </div>
+          {/* <!-- Você pode adicionar mais imagens aqui, o layout não será quebrado --> */}
+        </div>
+        <button className="prev-button" disabled={isTransitioning}>&#10094;</button>
+        <button className="next-button" disabled={isTransitioning}>&#10095;</button>
+      </div>
+    </section>
+  );
 }
 
-export default MeuComponente;
+export default Carrousel;
