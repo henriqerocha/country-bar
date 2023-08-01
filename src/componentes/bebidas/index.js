b57fc82ffd345120aa4f1e981e3f66c0d2a2dbed
henriqerocha <44968212+henriqerocha@users.noreply.github.com>
2023-07-31 22:05:41 -0300

    arrumando erros

--- src/componentes/bebidas/index.js
+++ src/componentes/bebidas/index.js
@@ -6,30 +6,18 @@ export default function Bebidas(){
         <section id='bebidas'>
             <div className='container-bebidas'>
                 <div className='titulo-section'>
-<<<<<<< HEAD
-                    <h2>Drinks <img src='./assets/bebidas-emoticon.png'></img></h2>
+                    <h2>Drinks <img src='./assets/bebidas-emoticon.webp'></img></h2>
                     <p>Nós temos os melhores e mais saborosos drinks para seu evento, veja alguns de nossos principais drinks abaixo. Ah, e tem muito mais!!!</p>
                     <p>Solicite já nosso orçamento para seu evento.</p>
                     <div className='button'>
                         <a href='https://wa.link/q8kd44' target='_blank' className='btn-orcamento'>Solicitar orçamento</a>
-=======
-                    <h2>Bebidas <img src='./assets/bebidas-emoticon.webp' alt='emoticon bebida'></img></h2>
-                    <p>Nós temos as melhores e mais emocionantes bebidas para sua festa, veja algumas das nossas principais bebidas abaixo. Ah, e tem muito mais!!!</p>
-                    <p>Solicite já nosso orçamento para seu evento.</p>
-                    <div className='button'>
-                        <a href='#' className='btn-orcamento'>Solicitar orçamento</a>
->>>>>>> aa7c8fbfe0966736aa5a99577e1c42a6e340defa
                     </div>
                 </div>
                     
                 <div className='content-cards'>
                     <div className='card-bebidas'>
                         <div className='img-bebida'>
-<<<<<<< HEAD
                             <img src='./assets/bebida-1.webp'></img>  
-=======
-                            <img src='./assets/bebida1.webp' alt='caipirinha'></img>  
->>>>>>> aa7c8fbfe0966736aa5a99577e1c42a6e340defa
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Pina Colada</h3>
@@ -46,11 +34,7 @@ export default function Bebidas(){
 
                     <div className='card-bebidas row-reverse'>
                         <div className='img-bebida'>
-<<<<<<< HEAD
                             <img src='./assets/bebida-2.webp'></img>  
-=======
-                            <img src='./assets/bebida2.webp' alt='caipirinha'></img>  
->>>>>>> aa7c8fbfe0966736aa5a99577e1c42a6e340defa
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Fresh Green</h3>
@@ -67,11 +51,7 @@ export default function Bebidas(){
 
                     <div className='card-bebidas'>
                         <div className='img-bebida'>
-<<<<<<< HEAD
                             <img src='./assets/bebida-3.webp'></img>  
-=======
-                            <img src='./assets/bebida3.webp' alt='caipirinha'></img>  
->>>>>>> aa7c8fbfe0966736aa5a99577e1c42a6e340defa
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Mojito</h3>
@@ -89,11 +69,7 @@ export default function Bebidas(){
 
                     <div className='card-bebidas row-reverse'>
                         <div className='img-bebida'>
-<<<<<<< HEAD
                             <img src='./assets/bebida-4.webp'></img>  
-=======
-                            <img src='./assets/bebida4.webp' alt='caipirinha'></img>  
->>>>>>> aa7c8fbfe0966736aa5a99577e1c42a6e340defa
                         </div>
                         <div className='descricao-bebida'>
                             <h3>Atlântico</h3>
--- src/componentes/conteudo/index.js
+++ src/componentes/conteudo/index.js
@@ -10,11 +10,10 @@ export default function Conteudo(){
         
         <main>
             <aside className='icone-contato'>
-<<<<<<< HEAD
+
                 <a href='https://wa.link/q8kd44' target='_blank'><img src='./assets/wpp-icon.png'></img></a>
-=======
-                <a href='https://api.whatsapp.com/send?phone=5561991380680&text=Ol%C3%A1,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20Open%20Bar%20%F0%9F%8D%B9%20Meu%20evento%20%C3%A9%20dia%20:%20Quantidade%20de%20convidados%20:%20%20Local:%20%20Hor%C3%A1rio:%20Tipo%20de%20evento%20:%20Obs:' target='_blank'><img src='./assets/wpp-icon.webp' alt='icone whatsapp'></img></a>
->>>>>>> aa7c8fbfe0966736aa5a99577e1c42a6e340defa
+
+
             </aside>
             <Carrousel/>
             <Bebidas/>
